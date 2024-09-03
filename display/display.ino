b73f790c8b8db9d62bf1126fdb4ad42da7d98cb1
Josue Santamaria <josuexsanta@gmail.com>
2024-09-03 08:51:02 -0600

    'new'

---
+++ display/display.ino
@@ -0,0 +1,22 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(-1);
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  
+  display.display();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
